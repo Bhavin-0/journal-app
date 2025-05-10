96fc93d508a7feafeea880f0b1b783cf3af2317b
Bhavin-0 <bhavin.joshi@mitaoe.ac.in>
2025-05-10 15:44:46 +0530

    Added JournalController with CRUD endpoints

---
+++ src/main/java/com/journal/journalapp/controller/JournalController.java
@@ -0,0 +1,56 @@
+package com.journal.journalapp.controller;
+
+//handles https requests -> returns https response
+//controller talks to the service layer
+
+import com.journal.journalapp.model.JournalEntry;
+import com.journal.journalapp.service.JournalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController             //Makes this a REST API controller
+@RequestMapping("/journal")         //base path for all endpoints
+public class JournalController {
+    private final JournalService journalService;
+
+    @Autowired
+    public JournalController(JournalService journalService){
+        this.journalService = journalService;
+    }
+
+    //create new journal Entry
+    @PostMapping
+    public ResponseEntity<JournalEntry> createEntry(@RequestBody JournalEntry entry){
+        JournalEntry created = journalService.createEntry(entry.getTitle(),entry.getContent());
+        return ResponseEntity.ok(created);
+    }
+
+    //get all entries
+    @GetMapping
+    public ResponseEntity<List<JournalEntry>> getAllEntries(){
+        return ResponseEntity.ok(journalService.getAllEntries());
+    }
+
+    //Get a specific journal Entry by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<JournalEntry> getEntryById(@PathVariable Long id){
+        return journalService.getEntryById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    //delete Entry by Id
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteEntryById(@PathVariable Long id){
+       try {
+           journalService.deleteEntryById(id);
+           return ResponseEntity.ok("Entry deleted Successfully");
+       }catch(RuntimeException e){
+           return ResponseEntity.notFound().build();
+       }
+    }
+
+}
---
+++ src/main/java/com/journal/journalapp/service/JournalService.java
@@ -0,0 +1,43 @@
+package com.journal.journalapp.service;
+
+import com.journal.journalapp.model.JournalEntry;
+import com.journal.journalapp.repository.JournalEntryRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+//Business logic
+//tolks to the database
+public class JournalService {
+    private final JournalEntryRepository repository;
+
+    public JournalService(JournalEntryRepository repository){
+        this.repository = repository;
+    }
+
+    public JournalEntry createEntry(String title,String content){
+        JournalEntry entry = new JournalEntry();
+        entry.setTitle(title);
+        entry.setContent(content);
+        entry.setCreatedAt(LocalDateTime.now());
+        return repository.save(entry);
+    }
+
+    public List<JournalEntry> getAllEntries(){
+        return repository.findAll();
+    }
+
+    public Optional<JournalEntry> getEntryById(Long id){
+        return repository.findById(id);
+    }
+
+    public void deleteEntryById(Long id){
+        Optional<JournalEntry> optionalJournalEntry = repository.findById(id);
+        if(optionalJournalEntry.isPresent()){
+            repository.deleteById(id);
+        }else{
+            throw new RuntimeException("Entry with ID : "+id+" not found.");
+        }
+    }
+}
